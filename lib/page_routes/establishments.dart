af199aec27c745e4df9f6a7ebcd4b84506d6490a
angelcustodio3 <112701307+angelcustodio3@users.noreply.github.com>
2025-05-15 03:46:37 +0800

    appbar house and establishment

--- lib/page_routes/establishments.dart
+++ lib/page_routes/establishments.dart
@@ -6,12 +6,29 @@ class EstablishmentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Establishments")),
-      body: const Center(
-        child: Text(
-          "Welcome to Establishments",
-          style: TextStyle(fontSize: 24),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
         ),
+        title: const Text(
+          'Establishment',
+          style: TextStyle(color: Colors.white),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF0A4423), Color(0xFF7A1E1E)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text("Welcome to Establishment", style: TextStyle(fontSize: 24)),
       ),
     );
   }
