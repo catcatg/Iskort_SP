7a501e827b14ff3bd2fcbc06193bee3112b21d85
gelicat <ac.angelicacustodio@gmail.com>
2025-11-16 01:19:50 +0800

    added shadow on navcard

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   // SEARCH BAR WIDGET
-  Widget _GeneralSearchBar() {
+  Widget GeneralSearchBar() {
     return Container(
       //padding: const EdgeInsets.symmetric(horizontal: 16),
       decoration: BoxDecoration(
@@ -72,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                     borderRadius: BorderRadius.circular(15),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
+                        color: Colors.grey.withAlpha(24),
                         blurRadius: 3,
                         offset: const Offset(0, 2),
                       ),
@@ -375,7 +375,7 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(height: 25),
 
             // NEW SEARCH BAR
-            _GeneralSearchBar(),
+            GeneralSearchBar(),
 
             const SizedBox(height: 30),
 
